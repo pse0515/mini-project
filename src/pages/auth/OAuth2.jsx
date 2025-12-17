eb3c120e86eb6ff8efe4ffe5981f1defed6540d5
pse0515 <pse12345665@gmail.com>
2025-12-17 11:07:15 +0900

    미니프로젝트중

--- src/pages/auth/OAuth2.jsx
+++ src/pages/auth/OAuth2.jsx
@@ -1,20 +1,21 @@
 import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useMeQuery } from "../../queries/usersQueries";
 
 function OAuth2() {
     const navigate = useNavigate();
     const [ searchParams ] = useSearchParams();
     const accessToken = searchParams.get("accessToken")
 
-useEffect(() => {
-    console.log(accessToken);
     if (!!accessToken) {
-        alert("로그인 성공!");
-    } else {
-        alert("로그인 후 이용 바랍니다.");
-        navigate("/auth/login");
+        localStorage.setItem("AccessToken", accessToken);
     }
-}, [accessToken]);
+    const meQuery = useMeQuery();
+
+    useEffect(() => {
+        console.log(meQuery.data);
+        
+    }, [meQuery.data]);
 
     return <></>
 }
